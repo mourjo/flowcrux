de9240a1be520eca05b571fb23df96c6a8c709c9
Mourjo Sen <hello@mourjo.me>
2025-09-20 13:19:11 +0200

    Add default project files

---
+++ src/main/java/me/mourjo/flowcrux/FlowcruxApplication.java
@@ -0,0 +1,13 @@
+package me.mourjo.flowcrux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowcruxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowcruxApplication.class, args);
+	}
+
+}
